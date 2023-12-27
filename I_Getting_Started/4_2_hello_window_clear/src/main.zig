003a9d44fdde0d52900cafd11d18451ac6b290ed
Minjie Zha <minjiezha@gmail.com>
2023-12-27 07:02:02 -0800

    copy 4_1 to 4_2 and change it accordingly

---
+++ I_Getting_Started/4_2_hello_window_clear/src/main.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+pub fn main() !void {
+    _ = glfw.init(.{});
+    defer glfw.terminate();
+
+    const hints = glfw.Window.Hints{
+        .context_version_major = 3,
+        .context_version_minor = 3,
+        .opengl_profile = .opengl_core_profile,
+    };
+    const width = 800;
+    const height = 600;
+    const window = glfw.Window.create(width, height, "LearnOpenGL", null, null, hints) orelse {
+        std.log.err("Failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+
+    glfw.makeContextCurrent(window);
+
+    const proc: glfw.GLProc = undefined;
+    try gl.load(proc, glGetProcAddress);
+
+    gl.viewport(0, 0, @intCast(width), @intCast(height));
+    window.setFramebufferSizeCallback(frameBufferSizeCallback);
+
+    while (!window.shouldClose()) {
+        window.swapBuffers();
+        glfw.pollEvents();
+    }
+}
+
+fn glGetProcAddress(_: glfw.GLProc, proc: [:0]const u8) ?*const anyopaque {
+    return glfw.getProcAddress(proc);
+}
+
+fn frameBufferSizeCallback(_: glfw.Window, width: u32, height: u32) void {
+    gl.viewport(0, 0, @intCast(width), @intCast(height));
+}
