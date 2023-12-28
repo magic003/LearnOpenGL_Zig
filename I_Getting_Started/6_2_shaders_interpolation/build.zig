136d28967576a0358132e0d62540c8af51c47150
Minjie Zha <minjiezha@gmail.com>
2023-12-27 18:30:37 -0800

    shaders interpolation

---
+++ I_Getting_Started/6_2_shaders_interpolation/build.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "6_2_shaders_interpolation",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+
+    // Use mach-glfw
+    const glfw_dep = b.dependency("mach_glfw", .{
+        .target = exe.target,
+        .optimize = exe.optimize,
+    });
+    exe.addModule("mach-glfw", glfw_dep.module("mach-glfw"));
+    @import("mach_glfw").link(glfw_dep.builder, exe);
+
+    // Use OpenGL
+    exe.addModule("gl", b.createModule(.{
+        .source_file = .{ .path = "../../libs/opengl/gl33.zig" },
+    }));
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
