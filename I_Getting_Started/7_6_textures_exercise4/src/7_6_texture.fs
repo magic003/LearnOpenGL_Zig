b24d113035cd2c3fef9e6d71064a5df828bbe071
Minjie Zha <minjiezha@gmail.com>
2024-01-01 20:48:43 -0800

    finish 7_6 exercise4

---
+++ I_Getting_Started/7_6_textures_exercise4/src/7_6_texture.fs
@@ -0,0 +1,15 @@
+#version 330 core
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float mixValue;
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue);
+}
