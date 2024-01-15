9f1d5b10566eb3d5d4d2278fd377406610a9be38
Minjie Zha <minjiezha@gmail.com>
2024-01-15 12:45:44 -0800

    finish 15_4 exercise4

---
+++ II_Lighting/15_4_lighting_maps_exercise4/shaders/15_4_lighting_maps.fs
@@ -0,0 +1,46 @@
+#version 330 core
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D emission;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TextCoords;
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+out vec4 FragColor;
+
+void main()
+{
+    float ambientStrength = 0.1;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TextCoords)).rgb;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TextCoords)).rgb;
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TextCoords)).rgb;
+
+    vec3 emission = vec3(texture(material.emission, TextCoords)).rgb;
+
+    FragColor = vec4(ambient + diffuse + specular + emission, 1.0);
+}
