41a477be14863abce83bf9c578a7ba9fecd4ed50
Minjie Zha <minjiezha@gmail.com>
2024-01-15 15:16:05 -0800

    finish 16_3

---
+++ II_Lighting/16_3_light_casters_spot/shaders/16_3_light_casters.fs
@@ -0,0 +1,52 @@
+#version 330 core
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TextCoords;
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+out vec4 FragColor;
+
+void main()
+{
+
+    vec3 lightDir = normalize(light.position - FragPos);
+    float theta = dot(lightDir, normalize(-light.direction));
+
+    if (theta > light.cutOff) {
+        float ambientStrength = 0.1;
+        vec3 ambient = light.ambient * vec3(texture(material.diffuse, TextCoords));
+
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TextCoords));
+
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        vec3 specular = light.specular * spec * vec3(texture(material.specular, TextCoords));
+
+        FragColor = vec4(ambient + diffuse + specular, 1.0);
+    } else {
+         FragColor = vec4(light.ambient * texture(material.diffuse, TextCoords).rgb, 1.0);
+    }
+}
